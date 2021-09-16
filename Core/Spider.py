accd9a92ffb8c9ea5b8e86d845ae73a20eed2280
Chenjian Shi <chenjianshi47@gmail.com>
2021-09-17 01:33:48 +0200

    Fixed bugs
    Add scroll function for more data could be seen in the page, besides, fix the bugs that may makes the calculation of xpath of target day wrong

--- Core/Spider.py
+++ Core/Spider.py
@@ -23,12 +23,13 @@ class Spider(object):
         self.Download_directory = download_directory
         pass
 
+    # Initialize the settings of browser and open the Wayback Machine website.
     def open_browser(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('window-size=1920x1080')
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--headless')
         chrome_options.add_argument('blink-settings=imagesEnabled=false')
         chrome_options.add_experimental_option('detach', True)
         prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': self.Download_directory}
@@ -39,39 +40,63 @@ class Spider(object):
         self.driver.get(self.Wayback_Machine_URL)
         pass
 
+    # Input the website of the target webpage and press the enter for search.
     def open_website(self, target_url):
         self.driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[3]/form/div/div/input[1]'). \
             send_keys(target_url)
         self.driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[3]/form/div/div/input[1]').send_keys(Keys.ENTER)
         pass
 
+    # Choose the year of 2020
     def choose_year_2020(self):
         self.driver.find_element_by_xpath('/html/body/div[4]/div[3]/div/div[2]/span[25]').click()
         pass
 
+    # Choose the year of 2021
     def choose_year_2021(self):
         self.driver.find_element_by_xpath('/html/body/div[4]/div[3]/div/div[2]/span[26]').click()
         pass
 
-    def locate_jan_1st(self):
-        for i in range(1, 8):
-            xpath = '/html/body/div[4]/div[4]/div[2]/div[1]/div[2]/div[1]/div[' + str(i) + ']'
-            if self.driver.find_element_by_xpath(xpath).get_attribute('class') == 'month-blank-day-container':
-                continue
-                pass
-            else:
-                self.JAN_1ST_location = xpath
-                break
-                pass
-            pass
-        matchobj = re.search(r'/html/body/div\[4]/div\[4]/div\[2]/div\[1]/div\[2]/div\[1]/div\[(.*)]',
-                             self.JAN_1ST_location)
-        start = int(matchobj.group(1)) - 1
-        return start
+    # This function is used for generate the xpath date for the target date.
+    def xpath_for_target_date(self, target_date, year):
+        # Check the month of the target day
+        month = datetime.strptime(target_date, '%b %d %Y').strftime('%b')
+        # Check the start day of this month(Monday, etc.)
+        start_day = self.day_calender[datetime.strptime(month + ' 1 ' + str(year), '%b %d %Y').strftime('%A')] - 1
+        # Check the day(Monday,etc) of the target day
+        day = datetime.strptime(target_date, '%b %d %Y').strftime('%A')
+        # Calculates the days between the target day and the start of that month, like target_day is JUN 16, then
+        # calculate the days between JUN 16 and JUN 1, then plus with the start day that make the start from the first
+        # Sunday of the year, which is the start day in the Wayback Machine
+        days = int((datetime.strptime(target_date, '%b %d %Y') -
+                    datetime.strptime(month + ' 1 ' + str(year), '%b %d %Y')).days) + start_day
 
+        # Calculates the weeks that shows in the Wayback Machine
+        weeks = int(days / 7) + 1
+        # Generate the xpath data for target day
+        xpath = '/html/body/div[4]/div[4]/div[2]/div[' + str(self.month_calender[month]) + ']/div[2]/div[' + \
+                str(weeks) + ']/div[' + str(self.day_calender[day]) + ']'
+        print(xpath)
+        return xpath
+        pass
+
+    # This function is used for scroll the scroller
+    def scroll_the_page(self):
+        new_height = int(self.driver.execute_script("return document.body.scrollHeight")) / 3 * 2
+        js_script = 'window.scrollBy(0,' + str(new_height) + ')'
+        self.driver.execute_script(js_script)
+        pass
+
+    # This function is used for checking the xpath for latest time page of the target dates, if there is no time could
+    # be selected, this function would return the xpath as None
     def choose_time(self):
         xpath_last = None
         for i in range(1, 9):
+            # If there is only one time page could be selected in the target dates, the xpath would be like
+            # '/html/body/div[4]/div[4]/div[3]/div/div[2]/ul/div/div/li/a'.
+            # If there are multiple time(more than 1) could be choose, the xpath would be
+            # '/html/body/div[4]/div[4]/div[3]/div/div[2]/ul/div/div/li[i]/a', this i is depends on how many time could
+            # be choose.
             if i == 1:
                 try:
                     xpath = '/html/body/div[4]/div[4]/div[3]/div/div[2]/ul/div/div/li/a'
@@ -111,26 +136,17 @@ class Spider(object):
             pass
         return xpath_last, self.last_time
 
-    def xpath_for_target_date(self, target_date, year):
-        start = self.locate_jan_1st()
-        month = datetime.strptime(target_date, '%b %d %Y').strftime('%b')
-        day = datetime.strptime(target_date, '%b %d %Y').strftime('%A')
-        days = int((datetime.strptime(target_date, '%b %d %Y') -
-                    datetime.strptime(month + ' 1 ' + str(year), '%b %d %Y')).days) + start
-        weeks = int(days / 7) + 1
-        xpath = '/html/body/div[4]/div[4]/div[2]/div[' + str(self.month_calender[month]) + ']/div[2]/div[' + \
-                str(weeks) + ']/div[' + str(self.day_calender[day]) + ']'
-        return xpath
-        pass
-
+    # Download the excel file for swedish COVID-19 in Folkhalsomyndigheten
     def download_file(self):
         self.driver.find_element_by_link_text('Data som statistiken ovan bygger på kan laddas ner här (Excel)').click()
         pass
 
+    # This function is only used for click the accept button of cookies, so that it would not shade the download link
     def click_cookie_button(self):
         self.driver.find_element_by_id('cookies-button').click()
         pass
 
+    # Do the open select date time page and download the file, at last, close the browser
     def open_select_date_time_page_and_download(self, target_date):
         year = datetime.strptime(target_date, '%b %d %Y').strftime('%Y')
         if year == '2020':
@@ -139,6 +155,8 @@ class Spider(object):
         elif year == '2021':
             self.choose_year_2020()
             pass
+        self.driver.implicitly_wait(1)
+        self.scroll_the_page()
         self.driver.implicitly_wait(1)
         self.driver.find_element_by_xpath(self.xpath_for_target_date(target_date, year)).click()
         self.driver.implicitly_wait(1)
--- main.py
+++ main.py
@@ -5,19 +5,29 @@ from datetime import datetime
 from Config import *
 from Core.Spider import Spider
 
+print('Start to downloading files')
+
+# Generate the dates that have file to download
 dates = pd.date_range(start_date, final_date).strftime('%b %d %Y').to_list()
+# Initialize the Selenium
 Spider = Spider(chrome_driver=chrome_driver, wayback_machine_url=Wayback_Machine_URL,
                 download_directory=Download_directory)
+# Start a loop for downloading the file
 for target_date in dates:
+    # Checking the time whether is earlier than JUL 17 2020, if it is, use the old website, if not use the new one
     if datetime.strptime(target_date, '%b %d %Y').date() < datetime.strptime('JUL 17 2020', '%b %d %Y').date():
         Target_URL_N = Target_URL[0]
         pass
     else:
         Target_URL_N = Target_URL[1]
         pass
+    # Open a new browser
     Spider.open_browser()
+    # Search the past data in the Wayback Machine webpage
     Spider.open_website(target_url=Target_URL_N)
+    # Wait 10 seconds for web page loadding
     time.sleep(10)
+    # Open the lasted time pages in the select date and download the file
     Spider.open_select_date_time_page_and_download(target_date=target_date)
     pass
 
